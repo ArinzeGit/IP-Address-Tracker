a77ae0f7d007e826eadb302affb929a7a64b70ed
Arinze Owoh <arinzeowoh@gmail.com>
2024-05-10 04:53:22 +0100

    fix marker: use custom marker

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,5 +1,8 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; // Ensure Leaflet CSS is imported
+import "leaflet/dist/leaflet.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import markerShadowPng from "leaflet/dist/images/marker-shadow.png";
+import { Icon } from "leaflet";
 
 interface Props {
   latitude: number;
@@ -18,7 +21,19 @@ function MapComponent({ latitude, longitude }: Props) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[latitude, longitude]}>
+      <Marker
+        position={[latitude, longitude]}
+        icon={
+          new Icon({
+            iconUrl: markerIconPng,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            shadowUrl: markerShadowPng,
+            shadowSize: [41, 41],
+            shadowAnchor: [12, 41],
+          })
+        }
+      >
         <Popup>
           The coordinates of this position are <br /> {"{"}lat:{latitude}, lng:
           {longitude}
